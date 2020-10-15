1ecc9e1612a0a3747489b3c5eaed7401cc3dea8b
Allan <mail@allanreid.co>
2020-10-15 15:20:42 -0700

    Added tapped gesture

--- GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/ViewController.swift
@@ -15,9 +15,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
     }
 
+    @objc func changePic() {
+        
+        print("tapped")
+        
+    }
 
 }
 
